00fcc742bb8ace46ab51c621a924ed642cad3bfb
unknown <shyusupova4@gmail.com>
2022-10-26 15:34:17 +0500

    Update files

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+/* Animals */
 INSERT INTO animals (
     id,
     name,
@@ -34,8 +35,6 @@ INSERT INTO animals (
     weight_kg )
 VALUES (4, 'Devimon', '2017-05-12', 5, true, 11.0);
 
-/* update */
-
 INSERT INTO animals (
     id,
     name,
@@ -50,3 +49,47 @@ VALUES (5, 'Charmander', '2020-02-8', 0, false, -11.0),
 (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
 (10, 'Blossom', '1998-02-13', 3, true, 17.0),
 (11, 'Ditto', '2022-05-14', 4, true, 22.0);
+
+/* Owners */
+-- Insert data into owners table
+INSERT INTO owners ( full_name, age )
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+-- Modify owners_id column
+UPDATE animals
+SET owners_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owners_id = 2
+WHERE name IN ('Pikachu', 'Gabumon');
+
+UPDATE animals
+SET owners_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owners_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owners_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
+
+/* Species */
+-- Insert data into species table
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+
+-- Modify species_id column
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
